6ac44b4432b5459657721078d0f7f2c1c9a34a4d
Nata010424 <106608212+Nata010424@users.noreply.github.com>
2022-08-06 11:49:44 +0300

    Made wheather forecast, added api

--- srcc/index.js
+++ srcc/index.js
@@ -22,6 +22,52 @@ let day = days[now.getDay()];
 h2.innerHTML = day;
 h6.innerHTML = `${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span>
+            <span class="weather-forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
+          </div>
+          <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="50"
+        />
+        </div>
+   `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  document.querySelector("#forecast").innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function handlePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -38,6 +84,7 @@ let newButton = document.querySelector("#button-location");
 newButton.addEventListener("click", getLocation);
 
 function wheatherCity(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   celsiusTemperature = response.data.main.temp;
 
@@ -61,7 +108,9 @@ function wheatherCity(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = "46a90e4bf20000a35f74ecbd7bc09967";
   let units = "metric";
